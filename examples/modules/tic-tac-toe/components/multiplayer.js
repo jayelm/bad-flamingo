ba2a8ae77f4b17fb7830e256a7b7100a2828bf3c
Jesse Mu <jlm95@cam.ac.uk>
2018-01-20 17:19:04 +0000

    Make slightly cleaner

--- examples/modules/tic-tac-toe/components/multiplayer.js
+++ examples/modules/tic-tac-toe/components/multiplayer.js
@@ -22,11 +22,9 @@ const Multiplayer = () => (
   <div style={{ padding: 0 }}>
     <div className="run">
       <App gameID="multi" playerID="0" />
-      &lt;App playerID=&quot;0&quot;/&gt;
     </div>
     <div className="run">
       <App gameID="multi" playerID="1" />
-      &lt;App playerID=&quot;1&quot;/&gt;
     </div>
   </div>
 );
